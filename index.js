d649d28780055a0a06e4d5fddfb9d2a9ac3e310c
AlePini <djpini00@gmail.com>
2020-10-05 12:51:43 +0000

    Added DASH video only and Q for submitting posts

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const fs = require('fs');
 const ini = require('ini');
 const mysql = require('mysql');
+const queue = require('queue-fifo');
 const snoowrap = require('snoowrap');
 const telegraf = require('telegraf');
 const ytdlWrap = require("youtube-dl-wrap");
 const ytdl = new ytdlWrap("/usr/bin/youtube-dl");
 
 const vidDir = "vid/"
+var Q = new queue();
 
 function getExtension(filename) {
   var i = filename.lastIndexOf('.');
@@ -28,7 +30,7 @@ function downloadDASH(url){
     fs.existsSync(output) && fs.unlinkSync(output);
 
     ytdl.exec([url,
-      "-f", "bestvideo+bestaudio", "-o", output])
+      "-f", "bestvideo+bestaudio/bestvideo", "-o", output])
     .on("error", (exitCode, processError, stderr) => 
       reject("An error occured",  exitCode,  processError, stderr))
     .on("close", () => {
@@ -37,64 +39,63 @@ function downloadDASH(url){
 
 }
 
-var submit = function submit(){
-  
-  var time = 0;
+async function submit(){
+    while(!Q.isEmpty()){
+        var e = Q.peek(); Q.dequeue();
+        if ( ['.gif'].includes(getExtension(e.url)) ) {
+            console.log(" * Sending gif ... " + e.id);
+            tbot.telegram.sendAnimation(conf.Telegram.channelid, e.url);
+        } else if ( ['.png','.jpg','.jpeg'].includes(getExtension(e.url)) ) {
+            console.log(" * Sending pic ... " + e.id);
+            tbot.telegram.sendPhoto(conf.Telegram.channelid, e.url);
+        } else if ( ['.mp4'].includes(getExtension(e.url)) ) {
+            console.log(" * Sending vid ... " + e.id);
+            tbot.telegram.sendVideo(conf.Telegram.channelid, e.url);
+        } else if ( e.url.includes('https://v.redd.it/') ) {
+            console.log(" * Sending DASH vid ... " + e.id);
+            downloadDASH(e.url).then(
+                file => { 
+                console.log(file);
+                tbot.telegram.sendVideo(conf.Telegram.channelid, { source : file }).then( () => fs.unlinkSync(file));
+            });
+        }
+        await sleep(1);
+    }
+}
+
+var fetch = function fetch(){
 
   // _ Get Last Submissions
   r.getMe().getUpvotedContent({limit: parseInt( conf.Reddit.upvoteLimit ), skipReplies: true}).then(list => {
     
-    list.forEach( async e => {
-      
-      // Wait at least 1 sec between each loop
-      time++;
-      await sleep(time);
-      
+    list.forEach( e => {
       // Ask to db if the submission is already been checked
       upvoted.query("SELECT * FROM upvoted WHERE id LIKE ?", [e.id], function (err, result) {
         if (err) throw err;
 
         // If it doesn't show up on the db, then insert into it and then PUSH TO TELEGRAM
-        if ( !result.length ) upvoted.query("INSERT INTO upvoted (id) VALUES (?)", [e.id], function (err, result){
-          if (err) throw err;
-          console.log("- Added new submission: " + e.id);
-          // TODO - Telegram RePost Bot
-          if ( ['.gif'].includes(getExtension(e.url)) ) {
-            console.log(" * Sending gif ...");
-            tbot.telegram.sendAnimation(conf.Telegram.channelid, e.url);
-          } else if ( ['.png','.jpg','.jpeg'].includes(getExtension(e.url)) ) {
-            console.log(" * Sending pic ...");
-            tbot.telegram.sendPhoto(conf.Telegram.channelid, e.url);
-          } else if ( ['.mp4'].includes(getExtension(e.url)) ) {
-            console.log(" * Sending vid ...");
-            tbot.telegram.sendVideo(conf.Telegram.channelid, e.url);
-          } else if ( e.url.includes('https://v.redd.it/') ) {
-            console.log(" * Sending DASH vid ...");
-            downloadDASH(e.url).then(
-              file => { 
-                console.log(file);
-                tbot.telegram.sendVideo(conf.Telegram.channelid, { source : file }).then( () => fs.unlinkSync(file));
-              });
-          }
-        });
-        else {
-          console.log("- Already exists: " + e.id + " _ " + e.url);
-        }
+        if ( !result.length )
+            upvoted.query("INSERT INTO upvoted (id) VALUES (?)", [e.id], function (err, result){
+              if (err) throw err;
+              console.log("- Added new submission: " + e.id);
+              Q.enqueue(e);
+            });
+        else
+            console.log("- Already exists: " + e.id + " _ " + e.url);
       });
 
     });
 
   });
 
-  setTimeout(submit, 60000);
+  submit();  
+  setTimeout(fetch, 60000);
 
 }
 
 // _ Setup Telegram Bot
 var conf = ini.decode(fs.readFileSync('./RePost.ini', 'utf-8'));
 var tbot = new telegraf(conf.Telegram.token);
-// TODO - IP Command
-tbot.launch();
 
 // _ Setup reddit api
 var r = new snoowrap({
@@ -113,11 +114,13 @@ var upvoted = mysql.createConnection({
   database: conf.DB.database
 });
 
+// _ LAUNCH
+tbot.launch();
 upvoted.connect(function(err) {
   
   if (err) throw err;
   console.log("Connected!");
 
-  submit();
+  fetch();
 
 });
